e0cd8643af143cac0fd4c904554c3652904b0475
MuaazFarooqKhan <kmuaaz18@gmail.com>
2022-09-08 10:49:40 +0500

    Apollo using add command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,40 @@ import * as exporting from 'highcharts/modules/exporting.src';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LineChartComponent
-  ],
+  declarations: [AppComponent, LineChartComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonModule,
     NgChartsModule,
     HighchartsChartModule,
-    ChartModule
+    ChartModule,
+    ApolloModule,
+    HttpClientModule,
+    GraphQLModule,
   ],
   providers: [
-    { provide: HIGHCHARTS_MODULES, useFactory: () => [ more, exporting ] } // add as factory to your providers
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [more, exporting] }, // add as factory to your providers
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://48p1r2roz4.sse.codesandbox.io',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
